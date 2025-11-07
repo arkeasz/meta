40b0ddeb74305614afc621cfb0c9423499251412
arki <antoniorafaelariasromero@gmail.com>
2025-11-07 11:24:57 -0500

    delete unnecesary functions

--- functions/main.f90
+++
@@ -1,9 +0,0 @@
-module root_mod
-    use iso_c_binding
-contains
-    function f(x) result(y) bind(C, name="f")
-        real(c_double), value :: x
-        real(c_double) :: y
-        y = x**2 - 2.0_c_double  
-    end function f
-end module root_mod
